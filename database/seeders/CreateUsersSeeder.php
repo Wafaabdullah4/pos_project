c42d06df439c8c2b8dfb47c38113fc451b02a27e
Wafa Abdullah Nurbani Adam <you@example.com>
2023-09-05 21:07:13 +0700

    first commit

---
+++ database/seeders/CreateUsersSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class CreateUsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        $users = [
+            [
+                'name' => 'Admin User',
+                'kelas' => '',
+                'jk' => 'Laki-Laki',
+                'email' => 'admin@gmail.com',
+                'type' => 1,
+                'password' => bcrypt('123456'),
+            ],
+            [
+                'name' => 'Awaysha Cessarmana',
+                'kelas' => 'XII Rekayasa Perangkat Lunak',
+                'jk' => 'Laki-Laki',
+                'email' => 'awaysha@gmail.com',
+                'type' => 2,
+                'password' => bcrypt('awaysha123'),
+            ],
+            [
+                'name' => 'Muhammad Rafli',
+                'kelas' => 'XII Rekayasa Perangkat Lunak',
+                'jk' => 'Laki-Laki',
+                'email' => 'Rafli@gmail.com',
+                'type' => 0,
+                'password' => bcrypt('rafli123'),
+            ],
+        ];
+
+        foreach ($users as $key => $user) {
+            User::create($user);
+        }
+    }
+}
---
+++ resources/views/kurirHome.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    {{ Auth::user()->name }}
+                    <br>
+                    {{ Auth::user()->kelas }}
+                    <br>
+                    {{ Auth::user()->jk }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
